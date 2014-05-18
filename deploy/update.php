b686366995d5ddc3c9540c1f0a7b78400a59732f
Evelyn Hung <jj.evelyn@gmail.com>
2014-05-19 01:29:45 +0800

    this php file won't be directly called from extenal

--- deploy/update.php
+++ deploy/update.php
@@ -1,13 +1,6 @@
 <?php
 include_once("config.php");
 
-header('Content-Type: text/plain');
-setlocale(LC_ALL, 'en_US.UTF-8');
-
-if (!isset($argv) && $_SERVER['REQUEST_METHOD'] !== 'POST') {
-	die("Error: Not a POST request nor run from command line.");
-}
-
 if ( trim(exec('whoami')) !== RUNNING_USER ) {
 	die("Error: Please run with the specified user: ".RUNNING_USER);
 }
